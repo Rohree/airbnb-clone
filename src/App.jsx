1f55d1b0ef9563ec993bc73532b770ce1e01fe12
rohree <hellororyy880@gmail.com>
2024-10-08 05:56:00 +0200

    AirBNB -Static React clone

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import Hero from "./components/Hero.jsx"
+import NavBar from "./components/NavBar.jsx"
+import Card from "./components/card.jsx"
+import data from "./components/data.jsx"
+
+
+function App() {
+  const cards = data.map(item => {
+    return (
+      <Card
+        img = {item.coverImg}
+        rating = {item.stats.rating}
+        reviewCount = {item.stats.reviewCount}
+        location = {item.location}
+        title = {item.title}
+        price = {item.price}
+      />
+    )
+  })
+  return (
+    <>
+      <NavBar/>
+      <Hero />
+      <section className="card--list">
+          {cards}
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function NavBar(){
+    return (
+        <nav>
+            <img src="./src/images/airbnb-logo.png" alt="logo" />
+        </nav>
+    )
+    
+}
+
+export default NavBar
